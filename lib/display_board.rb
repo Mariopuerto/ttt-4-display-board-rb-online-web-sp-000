2bbdab80a5902f4ed9171558fbe32eb18304ccef
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-05-12 16:39:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," ","x"," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(row)
 puts "   |   |   "
 puts "-----------"
-puts "   |   |   "
+puts "   | x |   "
 puts "-----------"
 puts "   |   |   "
 end
